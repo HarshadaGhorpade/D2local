0b5151229723a3f5d0e9bc5a27479ff247d8f48b
HarshadaGhorpade <harshada2221@gmail.com>
2024-11-27 13:38:12 +0530

    2nd commit

--- p2.py
+++ p2.py
@@ -1,1 +1,19 @@
 # file 2 in new Demo
+""" git commit -m "p2"
+[master (root-commit) a0149d1] p2
+ 1 file changed, 1 insertion(+)
+ create mode 100644 p2.py
+
+PS D:\demo_git\demo\newD> git remote add origin https://github.com/HarshadaGhorpade/D2local.git
+
+PS D:\demo_git\demo\newD> git remote -v
+
+origin  https://github.com/HarshadaGhorpade/D2local.git (fetch)
+origin  https://github.com/HarshadaGhorpade/D2local.git (push)
+
+PS D:\demo_git\demo\newD> git branch
+* master 
+
+PS D:\demo_git\demo\newD> git branch -M main2
+PS D:\demo_git\demo\newD> git push origin main2
+"""
